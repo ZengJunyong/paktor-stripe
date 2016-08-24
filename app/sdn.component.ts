7214c7e47c2a20bd0765cadd77207676677d0602
yong <zengjunyong@gmail.com>
2016-08-24 11:08:11 +0800

    fill the data for SDN

--- app/sdn.component.ts
+++ app/sdn.component.ts
@@ -1,20 +1,33 @@
-import {Component, Input, OnInit, OnDestroy} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Payment} from './payment'
+import {StripeTableModel} from './stripe.table.model'
+import {Plan} from './plan'
+import {count} from "rxjs/operator/count";
 
 @Component({
   moduleId: module.id,
-  selector: 'my-hero-detail',
-  templateUrl: 'sdn.component.html',
+  templateUrl: 'price.table.html'
 })
-export class SDNComponent implements OnInit, OnDestroy {
-
-  ngOnInit() {
-
+export class SDNComponent extends StripeTableModel {
+  constructor() {
+    this.payments = [
+      {amount: 330, itemName: 'GaiGai 1-dates package'},
+      {amount: 1485, itemName: 'GaiGai 3-dates package'},
+      {amount: 1665, itemName: 'GaiGai 5-dates package'},
+      {amount: 2205, itemName: 'GaiGai 10-dates package'}
+    ];
+    this.plans = {
+      '3': [
+        {amount: 495, itemName: '3 dates package - 10% discount - 3 months (SDN Promo)', count: 3},
+        {amount: 555, itemName: '5 dates package - 10% discount - 3 months (SDN Promo)', count: 3},
+        {amount: 735, itemName: '10 dates package - 10% discount - 3 months (SDN Promo)', count: 3}
+      ],
+      '6': [
+        {amount: 247.5, itemName: '3 dates package - 10% discount - 6 months (SDN Promo)', count: 6},
+        {amount: 277.5, itemName: '5 dates package - 10% discount - 6 months (SDN Promo)', count: 6},
+        {amount: 367.5, itemName: '10 dates package - 10% discount - 6 months (SDN Promo)', count: 6}
+      ]
+    };
   }
-
-  ngOnDestroy() {
-  }
-
-
 }
